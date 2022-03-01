4bd9f6f2b18f5852e23e37b6e43dedd52147b8f3
rahad2694 <rahad.haw@gmail.com>
2022-03-01 22:54:00 +0600

    api data added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const searchInputBox = document.getElementById('search-input-box');
 
 
-const searchBtn = () =>{
-    console.log('clicked');
+const searchBtn =async() =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInputBox.value}`);
+    const data = await res.json();
+    displayResult(data.data);
+}
+
+const displayResult = (phones) =>{
+    console.log(phones);
 }
