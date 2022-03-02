ed4f6c1b77c47b6d901ce597d6002a09f8a73b4d
rahad2694 <rahad.haw@gmail.com>
2022-03-02 08:07:19 +0600

    hover effect added

--- js/app.js
+++ js/app.js
@@ -13,13 +13,15 @@ const displayResult = (phones) =>{
     phones.forEach(phone =>{
         console.log(phone);
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.className ='col d-flex justify-content-center';
         div.innerHTML=`
-        <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="card w-75 p-3 shadow rounded">
+            <div class="phone-image d-flex justify-content-center">
+                <img class="img-fluid" src="${phone.image}" class="card-img-top" alt="...">
+            </div>
+            <div class="card-body text-center">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">Brand: ${phone.brand}</p>
             </div>
         </div>
         `;
